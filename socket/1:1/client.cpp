14fa86e3fee86e01af6a0d7b8840db7cf7d8a284
leehaakjoo <l--h--j--@naver.com>
2021-11-07 14:40:54 +0900

    1:1 connect first commit

---
+++ socket/1:1/client.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+using namespace std;
+
+int main()
+{
+   
+    int client;
+    int portNum = 65456; // NOTE that the port number is same for both client and server
+    bool isExit = false;
+    int bufsize = 1024;
+    char buffer[bufsize];
+    char* ip = "127.0.0.1";
+
+    struct sockaddr_in server_addr;
+
+    client = socket(AF_INET, SOCK_STREAM, 0);
+
+
+    if (client < 0) 
+    {
+        cout << "\nError establishing socket..." << endl;
+        exit(1);
+    }
+
+    cout << "\n=> Socket client has been created..." << endl;
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(portNum);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+  
+
+    if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
+        cout << "=> Connection to the server " << inet_ntoa(server_addr.sin_addr) << " with port number: " << portNum << endl;
+
+    if (connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr)) == 0)
+        cout << "=> Connection to the server port number: " << portNum << endl;
+
+    cout << "=> Awaiting confirmation from the server..." << endl; //line 40
+    recv(client, buffer, bufsize, 0);
+    cout << "=> Connection confirmed, you are good to go...";
+
+    cout << "\n\n=> Enter # to end the connection\n" << endl;
+
+    // Once it reaches here, the client can send a message first.
+
+    do {
+        cout << "Client: ";
+        do {
+            cin >> buffer;
+            send(client, buffer, bufsize, 0);
+            if (*buffer == '#') {
+                send(client, buffer, bufsize, 0);
+                *buffer = '*';
+                isExit = true;
+            }
+        } while (*buffer != 42);
+
+        cout << "Server: ";
+        do {
+            recv(client, buffer, bufsize, 0);
+            cout << buffer << " ";
+            if (*buffer == '#') {
+                *buffer = '*';
+                isExit = true;
+            }
+
+        } while (*buffer != 42);
+        cout << endl;
+
+    } while (!isExit);
+
+    /* ---------------- CLOSE CALL ------------- */
+    /* ----------------- close() --------------- */
+
+    /* 
+        Once the server presses # to end the connection,
+        the loop will break and it will close the server 
+        socket connection and the client connection.
+    */
+
+    cout << "\n=> Connection terminated.\nGoodbye...\n";
+
+    close(client);
+    return 0;
+}
