5082d44a533e3fc75420f70b822507dce0efdd11
leehaakjoo <l--h--j--@naver.com>
2021-11-09 23:28:22 +0900

    add 1:N connect using thread

---
+++ socket/1:N/client.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+using namespace std;
+
+int main()
+{
+   
+    int client;
+    int portNum = 65457; // NOTE that the port number is same for both client and server
+    bool isExit = false;
+    int bufsize = 1024;
+    char buffer[bufsize];
+    char* ip = "127.0.0.1";
+
+    struct sockaddr_in server_addr;
+
+    client = socket(AF_INET, SOCK_STREAM, 0);
+
+
+    if (client < 0) 
+    {
+        cout << "\nError establishing socket..." << endl;
+        exit(1);
+    }
+
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(portNum);
+    server_addr.sin_addr.s_addr = inet_addr(ip);
+  
+
+    connect(client,(struct sockaddr *)&server_addr, sizeof(server_addr));
+
+    recv(client, buffer, bufsize, 0);
+    std::sprintf(buffer, "%d", portNum); //portNum을 보내줌.
+    send(client, buffer, bufsize, 0);
+    cout<< "echo-client is activated" << endl;
+
+    // Once it reaches here, the client can send a message first.
+
+    do {
+        cin >> buffer;
+        send(client, buffer, bufsize, 0);
+        if(strcmp(buffer, "quit") == 0 ){
+            isExit = true;
+            break;
+        }
+        recv(client, buffer, bufsize, 0);
+        
+        cout<<"received: " << buffer << endl;
+
+    } while(!isExit);
+
+    cout << "echo-client is de-activated" <<endl;
+    close(client);
+    return 0;
+}
